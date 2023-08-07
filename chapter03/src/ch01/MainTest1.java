c2118eba7f126d8a53775bf7c4a6b388517c34f7
digiiy <jinwook1244@example.com>
2023-08-07 12:29:33 +0900

    8월7일수업

--- chapter03/src/ch01/MainTest1.java
+++ chapter03/src/ch01/MainTest1.java
@@ -3,8 +3,10 @@ package ch01;
 public class MainTest1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World ~");
+		
 
-	}
+	} // end of main
 
-}
+} // end of class
